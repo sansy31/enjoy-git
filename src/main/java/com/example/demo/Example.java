91d511ddf8d64b255a9631f8b367d667e3a20b8a
wenjinming <13001910@qq.com>
2021-04-28 18:44:05 +0800

    V0.6

--- src/main/java/com/example/demo/Example.java
+++ src/main/java/com/example/demo/Example.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Example {
     @RequestMapping("/")
     String home(){
-        return"Hello git -V0.1";
+        return"Hello git -V0.6";
     }
 
     public static void main(String[] args) {
